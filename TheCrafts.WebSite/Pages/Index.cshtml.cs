d863f04d5fa56f03b06b3a28e6993699faaa0a08
Rachel <Rachel@DESKTOP-7LNVHQ0>
2022-09-27 22:04:12 -0500

    Add project files.

---
+++ TheCrafts.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheCrafts.WebSite.Models;
+using TheCrafts.WebSite.Services;
+
+namespace TheCrafts.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
